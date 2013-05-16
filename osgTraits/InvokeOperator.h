e449ed0212b4cdb12c7a11ea58c3c95c73e80e14
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-16 17:43:31 -0500

    formatting

--- osgTraits/InvokeOperator.h
+++ osgTraits/InvokeOperator.h
@@ -57,13 +57,16 @@ namespace osgTraits {
 	using invoke_detail::get_operation_invoker;
 
 	template<typename Operation>
-	typename get_operation_invoker<Operation>::return_type invokeOperation(typename get_operation_argument_c<Operation, 0>::type const& a) {
+	typename get_operation_invoker<Operation>::return_type
+	invokeOperation(typename get_operation_argument_c<Operation, 0>::type const& a) {
 		typedef typename boost::mpl::apply<get_operation_invoker<Operation> >::type Invoker;
 		return Invoker::performOperation(a);
 	}
 
 	template<typename Operation>
-	typename get_operation_invoker<Operation>::return_type invokeOperation(typename get_operation_argument_c<Operation, 0>::type const& a, typename get_operation_argument_c<Operation, 1>::type const& b) {
+	typename get_operation_invoker<Operation>::return_type
+	invokeOperation(typename get_operation_argument_c<Operation, 0>::type const& a,
+	                typename get_operation_argument_c<Operation, 1>::type const& b) {
 		typedef typename boost::mpl::apply<get_operation_invoker<Operation> >::type Invoker;
 		return Invoker::performOperation(a, b);
 	}
